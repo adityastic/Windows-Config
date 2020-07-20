793169c1e14decf499b18a7e1c17fdc28935a594
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-07-20 11:28:56 +0100

    chore: improved script

---
+++ Chocolatey-Conf.ps1
@@ -0,0 +1,34 @@
+
+function InstallChocolatey() {
+    Write-Host "Installing Chocolatey"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))    
+}
+
+function ConfigureGlobalConfirmation() {
+    Write-Host "Chocolatey - Configure Global Affirmation"
+    choco feature enable -n allowGlobalConfirmation    
+}
+
+function ScheduleDailyJob() {
+    Import-Module PSScheduledJob
+    Write-Host "Chocolatey - Create Task to upgrade all packages daily"
+    $ScheduledJob = @{
+        Name = "Chocolatey StartUp Upgrade"
+        ScriptBlock = {choco upgrade all -y}
+        Trigger = New-JobTrigger -AtStartup -RandomDelay 00:02:00
+        ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork
+    }
+    Register-ScheduledJob @ScheduledJob
+}
+
+function InstallAllPackages(){
+    Write-Host "Chocolatey - Install Basic Packages"
+    choco install git autohotkey vscode vlc choco install powertoys microsoft-windows-terminal
+}
+
+function SetupChocolatey() {
+    InstallChocolatey
+    ConfigureGlobalConfirmation
+    ScheduleDailyJob
+    InstallAllPackages
+}
--- Setup-Conf.ps1
+++ Setup-Conf.ps1
@@ -1,3 +1,5 @@
+. "$PSScriptRoot\Chocolatey-Conf.ps1"
+
 Write-Host "
   ___       _  _  _                  _        _____                 __ 
  / _ \     | |(_)| |                ( )      /  __ \               / _|
@@ -9,21 +11,4 @@ Write-Host "
                       |___/                                          
 " 
 
-Write-Host "Installing Chocolatey"
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-Write-Host "Chocolatey - Configure Global Affirmation"
-choco feature enable -n allowGlobalConfirmation
-
-Write-Host "Chocolatey - Create Task to upgrade all packages daily"
-Import-Module PSScheduledJob
-$ScheduledJob = @{
-    Name = "Chocolatey StartUp Upgrade"
-    ScriptBlock = {choco upgrade all -y}
-    Trigger = New-JobTrigger -AtStartup -RandomDelay 00:02:00
-    ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork
-}
-Register-ScheduledJob @ScheduledJob
-
-Write-Host "Chocolatey - Install Basic Packages"
-choco install git autohotkey vscode vlc choco install powertoys microsoft-windows-terminal
+SetupChocolatey
