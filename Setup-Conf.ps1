87b88a37d652f3ce403e4d815a819f7833c7d837
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-07-20 11:04:58 +0100

    chore: conf global affirmation

--- Setup-Conf.ps1
+++ Setup-Conf.ps1
@@ -11,3 +11,6 @@ Write-Host "
 
 Write-Host "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Write-Host "Chocolatey - Configure Global Affirmation"
+choco feature enable -n allowGlobalConfirmation
