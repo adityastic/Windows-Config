29fe5960aef0a9fdc228d7b695d4e6563b58c2b4
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-07-22 16:02:57 +0100

    chore: added check for choco before upgrade

--- Chocolatey-Conf.ps1
+++ Chocolatey-Conf.ps1
@@ -1,7 +1,7 @@
 
 function InstallChocolatey() {
     Write-Host "Installing Chocolatey"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))    
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))    
 }
 
 function ConfigureGlobalConfirmation() {
@@ -15,7 +15,7 @@ function ScheduleDailyJob() {
     Write-Host "Chocolatey - Create Task to upgrade all packages daily"
     $ScheduledJob = @{
         Name = "Chocolatey StartUp Upgrade"
-        ScriptBlock = {choco upgrade all}
+        ScriptBlock = { try { choco -v } catch { Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) } finally { choco upgrade all } }
         Trigger = New-JobTrigger -AtStartup
         ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork -StartIfOnBattery -ContinueIfGoingOnBattery
     }
