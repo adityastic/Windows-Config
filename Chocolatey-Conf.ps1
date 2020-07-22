8393459c7ac4f92d47fc010c3bfff76427210c3a
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-07-22 16:53:44 +0100

    fix: startup task scheduler

--- Chocolatey-Conf.ps1
+++ Chocolatey-Conf.ps1
@@ -15,13 +15,13 @@ function ScheduleDailyJob() {
     Write-Host "Chocolatey - Create Task to upgrade all packages daily"
     $ScheduledJob = @{
         Name = "Chocolatey StartUp Upgrade"
-        ScriptBlock = { try { choco -v } catch { Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) } finally { choco upgrade all } }
+        ScriptBlock = { Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) }
         Trigger = New-JobTrigger -AtStartup
         ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork -StartIfOnBattery -ContinueIfGoingOnBattery
     }
     Register-ScheduledJob @ScheduledJob
     $task = Get-ScheduledTask -TaskName "Chocolatey StartUp Upgrade"
-    $task.Principal = New-ScheduledTaskPrincipal -UserId "$($env:USERDOMAIN)\$($env:USERNAME)" -LogonType ServiceAccount
+    $task.Principal = New-ScheduledTaskPrincipal -UserId "$($env:USERDOMAIN)\$($env:USERNAME)" -LogonType ServiceAccount -RunLevel Highest
     Set-ScheduledTask $task
 }
 
