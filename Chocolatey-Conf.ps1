38ae4a431c5843cf031c68974235acf53164e84f
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-07-23 09:46:15 +0100

    chore: open another shell to see the upgrade

--- Chocolatey-Conf.ps1
+++ Chocolatey-Conf.ps1
@@ -15,8 +15,8 @@ function ScheduleDailyJob() {
     Write-Host "Chocolatey - Create Task to upgrade all packages daily"
     $ScheduledJob = @{
         Name = "Chocolatey StartUp Upgrade"
-        ScriptBlock = { Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) }
-        Trigger = New-JobTrigger -AtStartup
+        ScriptBlock = {  Start-Process powershell -Verb RunAs -ArgumentList "choco upgrade all" }
+        Trigger = New-JobTrigger -AtLogOn -RandomDelay 00:02:00
         ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork -StartIfOnBattery -ContinueIfGoingOnBattery
     }
     Register-ScheduledJob @ScheduledJob
