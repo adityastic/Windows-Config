11efceeda934c753b0e360dcbe2d88d8336d6691
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-07-23 10:30:37 +0100

    chore: prompt before opening pages to install softwares

--- ExternalTools-Conf.ps1
+++ ExternalTools-Conf.ps1
@@ -1,16 +1,16 @@
 
 function OpenInstallationLinks() {
-    $urls = @(
-        # Visual Studio
-        "https://visualstudio.microsoft.com/vs/",
-        # Docker Desktop
-        "https://hub.docker.com/editions/community/docker-ce-desktop-windows/",
-        # Microsoft Edge
-        "https://www.microsoft.com/en-us/edge"
-    )
+    Write-Host "These softwares would have to be downloaded manually, dont install them yet. Just download the installers for now. Let's Start"
 
-    foreach($url in $urls){
-        Start-Process $url
+    $urls = @{
+        VisualStudio = "https://visualstudio.microsoft.com/vs/"
+        DockerDesktop = "https://hub.docker.com/editions/community/docker-ce-desktop-windows/"
+        MicrosoftEdge = "https://www.microsoft.com/en-us/edge"
+    }
+
+    foreach($url in $urls.GetEnumerator()){
+        Start-Process $url.Value
+        Read-Host -Prompt "Opened: $($url.Name), When done with the download feel free to press any key: "
     }
 }
 
